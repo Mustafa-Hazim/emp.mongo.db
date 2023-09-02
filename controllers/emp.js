4f036208b8e1f943f4f4a210db1ba386760bf942
Mustafa <hazimmustafa77@gmail.com>
2023-09-03 00:00:19 +0200

    get employees by salary

--- controllers/emp.js
+++ controllers/emp.js
@@ -19,3 +19,20 @@ export const addEmp = async (req, res) => {
 export const getAllEmps = async (req, res) => {
     res.send(await Employe.find())
 }
+
+export const getEmpBySalary = async (req, res) => {
+    // check if there value to get more than
+    if (!req.query.val)
+        return res.status(400).send("salary val is missed")
+    // convert the compare vallue to the integar value 
+    const val = parseInt(req.query.val)
+    // decide whice operator (more than / less than)
+    if (req.query.opr == 'moreThan')
+        return res.send(await Employe.find({ salary: { $gte: val } }))
+    // check if there is a operator (more than / less than)
+    if (req.query.opr == 'lessThan')
+        return res.send(await Employe.find({ salary: { $lte: val } }))
+
+    // default behivor return missed more deatils:
+    res.send('missed more data')
+}
